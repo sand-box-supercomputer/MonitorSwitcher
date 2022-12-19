ed1ddaee8f0996c20755cc7e241df8a36852781f
Huy Pham <huypham060398@gmail.com>
2022-12-19 21:18:08 +0200

    refactor code

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const API_KEY = "b2cad1cd-7eac-4b68-82ad-73aa086e2705"
-const VALID_IDS = ["15", "18"]
-
-const SET_MONITOR_CMD_PREFIX = `D:\\_Work\\ControlMyMonitor\\ControlMyMonitor.exe /SetValue "\\\\.\\DISPLAY1\\Monitor0" 60`
-
-function setMonitorToComputer(computerId) {
-    return nodeCmd.runSync(`${SET_MONITOR_CMD_PREFIX} ${computerId}`)
-}
-
 const nodeCmd = require('node-cmd');
 const express = require('express')
 const app = express()
+
+const API_KEY = "b2cad1cd-7eac-4b68-82ad-73aa086e2705"
+
+const VALID_SETTERS = {
+    macbook: () => setMonitorToMacbook(),
+    pc: () => setMonitorToPc(),
+}
+
+const MONITOR_MODES = {
+    DP: "15",
+    HDMI1: "17",
+    HDMI2: "18",
+}
+
+function setMonitorToPc() {
+    return [
+        nodeCmd.runSync(`D:\\_Work\\ControlMyMonitor\\ControlMyMonitor.exe /SetValue "\\\\.\\DISPLAY1\\Monitor0" 60 ${MONITOR_MODES.DP}`),
+        nodeCmd.runSync(`D:\\_Work\\ControlMyMonitor\\ControlMyMonitor.exe /SetValue "\\\\.\\DISPLAY2\\Monitor0" 60 ${MONITOR_MODES.DP}`),
+    ]
+}
+
+function setMonitorToMacbook() {
+    return [
+        nodeCmd.runSync(`D:\\_Work\\ControlMyMonitor\\ControlMyMonitor.exe /SetValue "\\\\.\\DISPLAY1\\Monitor0" 60 ${MONITOR_MODES.HDMI1}`),
+        nodeCmd.runSync(`D:\\_Work\\ControlMyMonitor\\ControlMyMonitor.exe /SetValue "\\\\.\\DISPLAY2\\Monitor0" 60 ${MONITOR_MODES.HDMI2}`),
+    ]
+}
 const port = 444
 
 app.post(`/switchToComputer/:id/${API_KEY}`, (req, res) => {
     let id = req.params.id
-    if (!VALID_IDS.includes(id)) {
+    if (!(id in VALID_SETTERS)) {
         res.status(404).json({ status: "invalid ID" })
         return
     }
 
-    let data = setMonitorToComputer(id)
+    let data = VALID_SETTERS[id]()
     console.log(data)
     res.status(200).json({ status: 'OK', data })
 })
